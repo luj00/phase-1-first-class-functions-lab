7a08d45c8ecf9555d1fbdb3a0fc94897ac6732c9
root <root@DESKTOP-598GE48.localdomain>
2023-10-05 15:32:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+// Define returnFirstTwoDrivers function
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  // Define returnLastTwoDrivers function
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  // Define selectingDrivers array
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  // Define createFareMultiplier function
+  function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  // Define fareDoubler function
+  const fareDoubler = createFareMultiplier(2);
+  
+  // Define fareTripler function
+  const fareTripler = createFareMultiplier(3);
+  
+  // Define selectDifferentDrivers function
+  function selectDifferentDrivers(drivers, func) {
+    return func(drivers);
+  }
+  
